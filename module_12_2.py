538957ba73551fbabdc30f3b0df5e014711b7fd5
AMacmep <cherkashinandrey@mail.ru>
2024-11-10 09:04:47 +0700

    0511

--- module_12_2.py
+++ module_12_2.py
@@ -26,15 +26,18 @@ class TournamentTest(unittest.TestCase):
     def test_tourne1(self):
         tourne_1 = Tournament(self.distans, self.usein, self.nik)
         result = tourne_1.start()
+        self.assertTrue(result[list(result.keys())[-1]] == 'Ник')
         self.all_results['test_tourne1'] = result
 
     def test_tourne2(self):
         tourne_2 = Tournament(self.distans, self.andrey, self.nik)
         result = tourne_2.start()
+        self.assertTrue(result[list(result.keys())[-1]] == 'Ник')
         self.all_results['test_tourne2'] = result
 
     def test_tourne3(self):
         tourne_3 = Tournament(self.distans, self.usein, self.andrey, self.nik)
         result = tourne_3.start()
+        self.assertTrue(result[list(result.keys())[-1]] == 'Ник')
         self.all_results['test_tourne3'] = result
 
